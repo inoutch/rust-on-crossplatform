181f0aab3dea07948f357c8e581e80b016f7849a
inoutch <akiandaki@gmail.com>
2020-11-20 19:16:39 +0900

    Fix to path

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,4 @@ cargo apk run || echo "Suppress signing error"
 # Issue for https://github.com/rust-windowing/android-ndk-rs/issues/76#issuecomment-698508327
 java -jar uber-apk-signer-1.1.0.jar --apks ./target/debug/apk/
 
-adb install -r /home/inoue/projects/github/rust-on-android/target/debug/apk/rust-on-android-aligned-debugSigned.apk
+adb install -r ./target/debug/apk/rust-on-android-aligned-debugSigned.apk
