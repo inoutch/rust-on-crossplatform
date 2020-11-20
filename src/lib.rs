2ced0baf35536af2b3cd82ff5d1024a1f7003810
inoutch <akiandaki@gmail.com>
2020-11-20 15:36:02 +0900

    Add iOS features

--- src/lib.rs
+++ src/lib.rs
@@ -13,8 +13,34 @@ pub fn launch_from_wasm() {
 
 #[cfg_attr(target_os = "android", ndk_glue::main(backtrace = "on"))]
 pub fn launch_from_android() {
+    std::env::set_var("RUST_LOG", "info");
     env_logger::init();
     common();
+}
+
+#[cfg(target_os = "ios")]
+pub fn launch_from_ios() -> std::os::raw::c_int {
+    std::env::set_var("RUST_LOG", "info");
+    env_logger::init();
+    common();
+    0
+}
+
+#[cfg(target_os = "ios")]
+#[no_mangle]
+pub extern "C" fn main_rs() -> std::os::raw::c_int {
+    stop_unwind(launch_from_ios)
+}
+
+#[cfg(target_os = "ios")]
+fn stop_unwind<F: FnOnce() -> T + std::panic::UnwindSafe, T>(f: F) -> T {
+    match std::panic::catch_unwind(f) {
+        Ok(t) => t,
+        Err(_) => {
+            println!("Attempt to Unwind out of rust code");
+            std::process::abort()
+        }
+    }
 }
 
 pub fn common() {
